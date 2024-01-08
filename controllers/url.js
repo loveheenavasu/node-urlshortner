c6059a9dbaf27c3ca5224f1fa1428222a1537c4a
loveheenavasu <trivediheenalove@gmail.com>
2024-01-08 18:25:11 +0530

    uninstall mongodb

--- controllers/url.js
+++ controllers/url.js
@@ -1,29 +1,58 @@
 const shortid = require("shortid");
-const URL = require("../models/url");
+// const URL = require("../models/url");
+const urlData = {};
+// async function handleGenerateNewShortURL(req, res) {
+//   const body = req.body;
+//   if (!body.url) return res.status(400).json({ error: "url is required" });
+//   const shortID = shortid();
+
+//   await URL.create({
+//     shortId: shortID,
+//     redirectURL: body.url,
+//     visitHistory: [],
+//   });
+
+//   return res.json({ id: shortID });
+// }
 
 async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "url is required" });
   const shortID = shortid();
 
-  await URL.create({
+  // Store data in-memory
+  urlData[shortID] = {
     shortId: shortID,
     redirectURL: body.url,
     visitHistory: [],
-  });
+  };
 
   return res.json({ id: shortID });
 }
 
+
+
+// async function handleGetAnalytics(req, res) {
+//   const shortId = req.params.shortId;
+//   const result = await URL.findOne({ shortId });
+//   return res.json({
+//     totalClicks: result.visitHistory.length,
+//     analytics: result.visitHistory,
+//   });
+// }
 async function handleGetAnalytics(req, res) {
   const shortId = req.params.shortId;
-  const result = await URL.findOne({ shortId });
+  const result = urlData[shortId];
+
+  if (!result) {
+    return res.status(404).json({ error: "Short URL not found" });
+  }
+
   return res.json({
     totalClicks: result.visitHistory.length,
     analytics: result.visitHistory,
   });
 }
-
 module.exports = {
   handleGenerateNewShortURL,
   handleGetAnalytics,
